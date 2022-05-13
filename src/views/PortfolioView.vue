0640d762213816dd869e9ae53e5d2b5208f574af
Dekpo-Wyna Yologaza <dw.yologaza@yahoo.fr>
2022-05-13 10:22:15 +0200

    Added pagination logic...

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,34 +1,64 @@
 <script setup>
 import { ref } from 'vue'
+
 const portfolio = ref(null);
 
 let page = 1;
-const nextPage = () => {
-  page++;
-  console.log('page:',page);
+
+const displayPage = ref(page);
+
+const nextPage = (way='') => {
+  switch (way){
+    case 'next':
+      page++;
+    break;
+    case 'prev':
+      page>1 ? page-- : null;
+    break;
+  }
+  displayPage.value = page;
+  console.log('page:', page);
   loadPic(page);
 }
 
-const loadPic = (page=1)=>{
-  fetch('http://127.0.0.1:8000/api/picture?page='+page+'&limit=6')
-  .then((res) => res.json())
-  .then((json) => {
-    portfolio.value = json;
-    console.log(json);
-  });
+const loadPic = (page = 1) => {
+  fetch('http://127.0.0.1:8000/api/picture?page=' + page + '&limit=6')
+    .then((res) => res.json())
+    .then((json) => {
+      portfolio.value = json;
+      console.log(json);
+    });
 }
 (loadPic)()
 </script>
 
 <template>
-<button @click="nextPage">Next Page</button>
-<!-- début container -->
+  <!-- début container -->
   <div class="container-fluid">
     <div class="row">
       <div class="col-12">
         <h1>Portfolio</h1>
       </div>
     </div>
+    <!-- début ligne pagination -->
+    <div class="row">
+      <div class="col-12">
+        <nav aria-label="Page navigation example">
+          <ul class="pagination justify-content-center">
+            <li v-bind:class="{'page-item': true, 'disabled': displayPage==1?true:false }" >
+              <a class="page-link" href="#" @click="nextPage('prev')">Previous</a>
+            </li>
+            <li class="page-item active"><a class="page-link" href="#">{{displayPage}}</a></li>
+            <li class="page-item"><a class="page-link" href="#">{{displayPage+1}}</a></li>
+            <li class="page-item"><a class="page-link" href="#">{{displayPage+2}}</a></li>
+            <li class="page-item">
+              <a class="page-link" href="#" @click="nextPage('next')">Next</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+    <!-- fin ligne pagination -->
     <!-- début ligne -->
     <div class="row">
       <!-- début colonne -->
@@ -48,7 +78,7 @@ const loadPic = (page=1)=>{
     </div>
     <!-- fin ligne -->
   </div>
-<!-- fin container -->
+  <!-- fin container -->
 </template>
 
 <style>
